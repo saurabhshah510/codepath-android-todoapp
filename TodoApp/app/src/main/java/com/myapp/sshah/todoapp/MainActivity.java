4d000ba71b64643b45114bebd5181542f36e3322
sshah1 <sshah0@walmartlabs.com>
2015-10-02 09:30:25 -0700

    Setting up the list adapter and displaying some dummy strings

--- TodoApp/app/src/main/java/com/myapp/sshah/todoapp/MainActivity.java
+++ TodoApp/app/src/main/java/com/myapp/sshah/todoapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.myapp.sshah.todoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvItems;
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvItems = (ListView)findViewById(R.id.lvItems);
+        populateArrayItems();
+        lvItems.setAdapter(itemsAdapter);
+    }
+
+    public void populateArrayItems(){
+        items = new ArrayList<>();
+        items.add("Item 1");
+        items.add("Item 2");
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
     }
 }
